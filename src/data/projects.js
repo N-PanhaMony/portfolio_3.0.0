c7baeddc4a067754b69839bc81fff1e78085097b
N-PanhaMony <neangpanhamony001@gmail.com>
2026-02-25 23:16:31 +0700

    feat: add Prisma Blog API project to portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,27 @@
-import bookStoreImage from "../assets/ProjectsImage/bookstore-crop.png";
+
+import blogApiImage from "../assets/ProjectsImage/Blog-api.png";
 import habitChallengeImage from "../assets/ProjectsImage/habitChallenge-16x9-moreTopSpace.png";
+import bookStoreImage from "../assets/ProjectsImage/bookstore-crop.png";
 
 export const projectsData = [
+
   {
-    name: "BookStore ",
-    status: "Completed",
-    description:
-      "A responsive book search application that integrates with the Google Books API. Implements debounced search input, dynamic API queries, loading states, error handling, and clean reusable component architecture.",
-    image: bookStoreImage,
-    technologies: [
-      "React",
-      "Vite",
-      "Google Books API",
-      "JavaScript"
-    ],
-    liveUrl: "https://bookstorev1-omega.vercel.app/",
-    githubUrl: "https://github.com/N-PanhaMony/BookStore",
-  },
+  name: "Express + Prisma Blog API",
+  status: "Ongoing",
+  description:
+    "A production-ready RESTful API built with Express 5, Prisma ORM, and PostgreSQL. Implements JWT authentication, role-based authorization (ADMIN vs USER), Zod request validation, and full CRUD operations for Posts, Tags, and Comments. Deployed with environment-based configuration and migration automation.",
+  image: blogApiImage,
+  technologies: [
+    "Node.js",
+    "Express 5",
+    "Prisma / PostgreSQL",
+    "JWT",
+    "Zod",
+    "Render"
+  ],
+  // liveUrl: "https://express-prisma-z4x3.onrender.com",
+  githubUrl: "https://github.com/N-PanhaMony/Express_prisma"
+},
 
   {
     name: "21-Day Habit Challenge App",
@@ -33,4 +38,21 @@ export const projectsData = [
     liveUrl: "https://atomic-habits-21days.vercel.app/",
     githubUrl: "https://github.com/N-PanhaMony/ATOMIC_HABITS",
   },
+  {
+    name: "BookStore ",
+    status: "Completed",
+    description:
+      "A responsive book search application that integrates with the Google Books API. Implements debounced search input, dynamic API queries, loading states, error handling, and clean reusable component architecture.",
+    image: bookStoreImage,
+    technologies: [
+      "React",
+      "Vite",
+      "Google Books API",
+      "JavaScript"
+    ],
+    liveUrl: "https://bookstorev1-omega.vercel.app/",
+    githubUrl: "https://github.com/N-PanhaMony/BookStore",
+  },
+  
+
 ];
